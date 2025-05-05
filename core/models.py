cb2b5230d209aedb88245008404a0418bf63fd6f
oluwafunbi <oluwafunbi.onaeko@pau.edu.ng>
2025-05-06 00:51:23 +0100

    F

--- core/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Session(models.Model):
-    session_id = models.CharField(max_length=120, unique=True)
-    phone_number = models.CharField(max_length=20)
-    text = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
--- core/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ussd_callback
-
-urlpatterns = [
-    path('ussd/', ussd_callback),
-]

24fd498727ec545e1ffc13abf907d7fe5cdfb5c1
oluwafunbi <oluwafunbi.onaeko@pau.edu.ng>
2025-05-06 10:29:07 +0100

    F

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,8 @@
 import africastalking
 
 # TODO: Initialize the SDK
-username = 'sandbox'  # use 'sandbox' for the test environment
-api_key = 'atsk_40303b14f9e27e8c0f08d18ea3b8327cdf8cb7f1f8c036d198ac38ee5aea7e27b0e6a746'  # your sandbox API key
+username = 'sandbox'  # Must be 'sandbox' for test environment
+api_key = 'atsk_40303b14f9e27e8c0f08d18ea3b8327cdf8cb7f1f8c036d198ac38ee5aea7e27b0e6a746'  # Your sandbox API key
 africastalking.initialize(username, api_key)
 
 class send_sms:
@@ -12,11 +12,11 @@ class send_sms:
     # TODO: Send Message
     def sending(self):
         # Set the numbers in international format
-        recipients = ["+2349013413496"]  # your test number
+        recipients = ["+2349013413496"]  # Replace with your number added in simulator
         # Set your message
         message = "Hey AT Ninja!"
         # Set your shortCode or senderId
-        sender = "AFRICASTKNG"  # use 'AFRICASTKNG' for sandbox environment
+        sender = "AFRICASTKNG"  # Required sender for sandbox
 
         try:
             response = self.sms.send(message, recipients, sender)
